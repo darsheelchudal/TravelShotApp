17ad4dd821bbb95de2ea1daff6b6a7e009037871
Darsheel Chudal <74804981+darsheelchudal@users.noreply.github.com>
2024-05-17 07:33:58 +0545

    Frontend & backend connected using api, data passed as json to ATLAS MONGODB

--- frontend/src/components/Form/Form.js
+++ frontend/src/components/Form/Form.js
@@ -1,8 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
 import Post from "../Posts/Post/Post";
+import FileBase from "react-file-base64";
+import { useDispatch } from "react-redux";
+import { createPosts } from "../../actions/posts";
 
 const Form = () => {
-  const handleSubmit = () => {};
+  const [postData, setPostData] = useState({
+    creator: "",
+    title: "",
+    message: "",
+    tags: "",
+    selectedFile: "",
+  });
+  const dispatch = useDispatch();
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    dispatch(createPosts(postData));
+  };
+  const clear = () => {};
   return (
     <>
       <div class="sm:w-[38rem] mx-auto my-2 overflow-hidden rounded-2xl bg-white shadow-lg sm:max-w-lg">
@@ -13,47 +28,75 @@ const Form = () => {
         </div>
 
         <div class="space-y-4 px-8 py-10">
-          <label class="block" for="name">
-            <input
-              class="w-full rounded-md border bg-white px-2 py-2 outline-none ring-blue-600 focus:ring-1"
-              type="text"
-              placeholder="Creator"
-              onChange={(e) => e.target.value()}
-            />
-          </label>
-          <label class="block" for="name">
-            <input
-              class="w-full rounded-md border bg-white px-2 py-2 outline-none ring-blue-600 focus:ring-1"
-              type="text"
-              placeholder="Title"
-              onChange={(e) => e.target.value()}
+          <form onSubmit={handleSubmit}>
+            <label class="block" for="Creator">
+              <input
+                class="w-full rounded-md border bg-white px-2 py-2 outline-none ring-blue-600 focus:ring-1"
+                type="text"
+                placeholder="Creator"
+                value={postData.creator}
+                onChange={(e) =>
+                  setPostData({ ...postData, creator: e.target.value })
+                }
+              />
+            </label>
+            <label class="block" for="Title">
+              <input
+                class="w-full rounded-md border bg-white px-2 py-2 outline-none ring-blue-600 focus:ring-1"
+                type="text"
+                placeholder="Title"
+                value={postData.title}
+                onChange={(e) =>
+                  setPostData({ ...setPostData, title: e.target.value })
+                }
+              />
+            </label>
+            <label class="block" for="Message">
+              <textarea
+                class="h-32 w-full rounded-md border bg-white px-2 py-2 outline-none ring-blue-600 focus:ring-1"
+                type="text"
+                placeholder="Message"
+                value={postData.message}
+                onChange={(e) =>
+                  setPostData({ ...setPostData, message: e.target.value })
+                }
+              ></textarea>
+            </label>
+            <label class="block" for="Tags">
+              <input
+                class="w-full rounded-md border bg-white px-2 py-2 outline-none ring-blue-600 focus:ring-1"
+                type="text"
+                placeholder="Tags"
+                value={postData.tags}
+                onChange={(e) =>
+                  setPostData({ ...setPostData, tags: e.target.value })
+                }
+              />
+            </label>
 
-            />
-          </label>
-          <label class="block" for="name">
-            <textarea
-              class="h-32 w-full rounded-md border bg-white px-2 py-2 outline-none ring-blue-600 focus:ring-1"
-              type="text"
-              placeholder="Message"
-              onChange={(e) => e.target.value()}
+            <div className="fileInput m-4">
+              <label
+                class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                for="file_input"
+              >
+                Upload file
+              </label>
+              <FileBase
+                type="file"
+                multiple={false}
+                onDone={({ base64 }) =>
+                  setPostData({ ...postData, selectedFile: base64 })
+                }
+              />
+            </div>
 
-            ></textarea>
-          </label>
-          <label class="block" for="name">
-            <input
-              class="w-full rounded-md border bg-white px-2 py-2 outline-none ring-blue-600 focus:ring-1"
-              type="text"
-              placeholder="Tags"
-              onChange={(e) => e.target.value()}
-
-            />
-          </label>
-          <button class="mt-4 rounded-full bg-blue-800 px-10 py-2 font-semibold text-white">
-            Submit
-          </button>
-          <button class="mx-10 mt-4 rounded-full bg-red-800 px-10 py-2 font-semibold text-white">
-            Clear
-          </button>
+            <button class="mt-4 rounded-full bg-blue-800 px-10 py-2 font-semibold text-white">
+              Submit
+            </button>
+            <button class="mx-10 mt-4 rounded-full bg-red-800 px-10 py-2 font-semibold text-white">
+              Clear
+            </button>
+          </form>
         </div>
       </div>
     </>
