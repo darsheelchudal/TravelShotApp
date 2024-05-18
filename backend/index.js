261a2ca0d1fe90bbe5eb255649832a3023b58b00
Darsheel Chudal <74804981+darsheelchudal@users.noreply.github.com>
2024-05-18 06:58:10 +0545

    Ready for deployment, env variable edited,

--- backend/index.js
+++ backend/index.js
@@ -3,6 +3,7 @@ import bodyParser from "body-parser";
 import cors from "cors";
 import express from "express";
 import postRoutes from "./routes/posts.js";
+import dotenv from "dotenv";
 
 const app = express();
 
@@ -12,13 +13,12 @@ app.use(cors());
 
 //localhost:5000/posts/
 app.use("/posts", postRoutes);
+dotenv.config();
 
-const CONNECTION_URL = `mongodb+srv://darsheelchudal11:darsheel@cluster0.3jiunuh.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
-
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT;
 
 mongoose
-  .connect(CONNECTION_URL)
+  .connect(process.env.CONNECTION_URL)
   .then(() =>
     app.listen(PORT, () => console.log(`Server running at Port : ${PORT}`))
   )
